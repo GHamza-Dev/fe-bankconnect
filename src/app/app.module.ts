e1246d6f4457d413041f84a0bc6618c91e8655bb
Ghamza-dev <hamza.gassai@gmail.com>
2023-01-25 14:37:25 +0100

    Create navigation bar and login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MainComponent } from './layout/main/main.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
